9ec21214bfe77abd7a25ce45dbc59eba99597e51
Niels Mesure <nielsmesure5@gmail.com>
2025-08-25 20:16:14 +0200

    Update navigation

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,7 +12,6 @@ export default function DashboardPage() {
         <ExpenseChart />
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           <div className="lg:col-span-2">
-
             <CalendarView />
           </div>
           <div>
